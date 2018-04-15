66dad0f0e86627993bad6e0e413f6d4d83ba9f0f
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-15 14:19:15 +0200

    Add general markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import { Timeline } from "./components/Timeline";
+import { TweetForm } from "./components/TweetForm";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <>
+        <header>
+          <h1>Kwetter</h1>
+
+          <nav />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+
+        <main>
+          <TweetForm />
+          <Timeline />
+        </main>
+      </>
     );
   }
 }
